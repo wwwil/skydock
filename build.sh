a8e6bd3f60a873052d7b1c2fa294b0cc4529daf8
William Squires <wil@edwar.do>
2018-11-16 16:54:34 +0000

    Create initial build script, this will just show the contents of the Raspbian boot directory to demonstrate that the script works

---
+++ build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Install dependencies
+apt-get update && apt-get install -y wget unzip qemu qemu-user-static binfmt-support
+
+IMG_LINK=http://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2018-10-11/2018-10-09-raspbian-stretch-lite.zip
+IMG_ZIP=$(basename $IMG_LINK)
+
+# Download zip and extract the img
+if [ ! -f "$IMG_ZIP" ]; then
+	wget $IMG_LINK
+fi
+unzip -o *.zip
+
+# Create loop device map from image partition table
+LOOP_DEV=$(losetup --show -f -P *.img)
+
+# Wait a second or mount may fail
+sleep 1
+
+# Make mount point and mount image
+mkdir -p /mnt/raspbian
+mount -o rw /dev/mapper/${LOOP_DEV}p2 /mnt/raspbian
+mount -o rw /dev/mapper/${LOOP_DEV}p1 /mnt/raspbian/boot
+
+# Create bind mounts for system directories
+mount --bind /dev /mnt/raspbian/dev/
+mount --bind /sys /mnt/raspbian/sys/
+mount --bind /proc /mnt/raspbian/proc/
+mount --bind /dev/pts /mnt/raspbian/dev/pts
+
+# Apply ld.so.preload fix
+sed -i 's/^/#CHROOT /g' /mnt/raspbian/etc/ld.so.preload
+
+# Copy qemu binary
+cp /usr/bin/qemu-arm-static /mnt/raspbian/usr/bin/
+
+# Chroot to raspbian
+chroot /mnt/raspbian ls /boot
+
+# Revert ld.so.preload fix
+sed -i 's/^#CHROOT //g' /mnt/raspbian/etc/ld.so.preload
+
+# Unmount everything
+umount /mnt/raspbian/{dev/pts,dev,sys,proc,boot,}
