e2e11e9e71d07ab005c64fb27ee2045329e22164
wwwil <wwwil.squires@gmail.com>
2019-09-14 18:00:39 +0100

    Add check to ensure that expand worked correctly

--- expand.sh
+++ expand.sh
@@ -8,8 +8,22 @@ set -o xtrace
 # Set EXPAND to first argument, or 0 if not provided
 EXPAND="${1:-0}"
 
+# Determine the current size of the partition
+SIZE_BEFORE=$(parted -s "${SOURCE_IMAGE}" unit Mib print | grep -e '^ 2' \
+| xargs echo -n | cut -d" " -f 4 | tr -d MiB)
+
 # Add EXPAND * 1M to the end of the image
 dd bs=1M if=/dev/zero count=$EXPAND >> $SOURCE_IMAGE
 
 # Resize the second partition to fill the free space
 parted -s "${SOURCE_IMAGE}" resizepart 2 100%
+
+# Determine the new size of the partition
+SIZE_AFTER=$(parted -s "${SOURCE_IMAGE}" unit Mib print | grep -e '^ 2' \
+| xargs echo -n | cut -d" " -f 4 | tr -d MiB)
+
+SIZE_DIFFERENCE=$(($SIZE_AFTER - $SIZE_BEFORE))
+if [ "$SIZE_DIFFERENCE" != "$EXPAND" ]; then
+    echo "Expand error, SIZE_BEFORE: $SIZE_BEFORE, SIZE_AFTER: $SIZE_AFTER, SIZE_DIFFERENCE: $SIZE_DIFFERENCE"
+    exit 1
+fi
