8b15e8cb931f1339634dd35fb16308e9fdf46f71
wwwil <wwwil.squires@gmail.com>
2019-09-14 17:38:24 +0100

    Correct name of env var in expand script

--- expand.sh
+++ expand.sh
@@ -9,7 +9,7 @@ set -o xtrace
 EXPAND="${1:-0}"
 
 # Add EXPAND * 1M to the end of the image
-dd bs=1M if=/dev/zero count=$EXPAND >> $IMG_FILE
+dd bs=1M if=/dev/zero count=$EXPAND >> $SOURCE_IMAGE
 
 # Resize the second partition to fill the free space
-parted -s "${IMG_FILE}" resizepart 2 100%
+parted -s "${SOURCE_IMAGE}" resizepart 2 100%
